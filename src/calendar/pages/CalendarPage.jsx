90bc96e004ef6a54a1a8bbcda1ea4a7f16b71fff
sebassg7 <sebastlopez@unicauca.edu.co>
2024-02-19 12:43:40 -0500

    Personalizar el cuadro de evento

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar  } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
-import { Navbar } from '../';
+import { CalendarEvent, Navbar } from '../';
 import { localizer, getMessages } from "../../helpers";
 
 const events = [{
@@ -20,7 +20,6 @@ const events = [{
 export const CalendarPage = () => {
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
-    console.log({ event, start, end, isSelected });
 
     const style = {
       backgroundColor: '#347CF7',
@@ -48,6 +47,9 @@ export const CalendarPage = () => {
         style={{ height: 'calc( 100vh - 80px)' }}
         messages={ getMessages() }
         eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent,
+        }}
       />
     </>
   )
