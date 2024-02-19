e69acf10f45d8c29aa09e7d615ab3984cb2c38cc
sebassg7 <sebastlopez@unicauca.edu.co>
2024-02-19 11:41:16 -0500

    React Big Calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,45 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US'
+
 import { Navbar } from '../';
 
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [{
+  title: 'Cumpleaños del Jefe',
+  notes: 'Hay que comprar pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: 'fafafa',
+  user:{
+    _id: '123',
+    name: 'Fernando',
+  }
+}];
 
 export const CalendarPage = () => {
   return (
     <>
-      <div className='navbar navbar-dark bg-dark mb-4 px-4'>
-        <span className='navbar-brand'>
-          <i className='fas fa-calendar-alt'></i>
-          &nbsp;
-          Sebastián
-        </span>
-
-        <button className='btn btn-outline-danger'>
-          <i className='fas fa-sign-out-alt' ></i>
-          <span>Salir</span>
-        </button>
-      </div>
+      <Navbar/>
+      <Calendar
+        localizer={ localizer }
+        events= { events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px)' }}
+      />
     </>
   )
 };
